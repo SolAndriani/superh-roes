7012e8ad9b681e2ff65fe6f3176e7c997aac9583
sol <solagustinaandriani@gmail.com>
2025-06-21 15:39:45 -0300

    arreglo de detalles

--- test/auth/types/types.test.js
+++ test/auth/types/types.test.js
@@ -1,16 +1,10 @@
 import { types } from '../../../src/auth/types/types';
 
-
-describe('Pruebas en "Types.js"', () => { 
-
-    test('debe de regresar estos types ', () => { 
-
-
-
-        expect(types).toEqual({
-            login: '[Auth] Login',
-            logout:'[Auth] Logout', 
-        })
-
-     });
- });
+describe('Pruebas en types.js', () => {
+  test('debe exportar los tipos de acción auth correctos', () => {
+    expect(types).toEqual({
+      login: '[Auth] Login',
+      logout: '[Auth] Logout',
+    });
+  });
+});
